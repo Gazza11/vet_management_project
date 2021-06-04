76546e8268505c000c79c9dfa3a9e2d383a4c5c5
Gary Darnell <gdarnell9@gmail.com>
2021-06-04 19:03:37 +0100

    Fixed bug in Animal class that added all notes for multiple animals together. Also added the function to add notes to an animal.

--- models/animal.py
+++ models/animal.py
@@ -1,8 +1,12 @@
 class Animal:
-    def __init__(self, name, date_of_birth, animal_type, owner_number, treatment_notes = [], current_vet = None):
+    def __init__(self, name, date_of_birth, animal_type, owner_number, current_vet = None):
         self.name = name
         self.date_of_birth = date_of_birth
         self.animal_type = animal_type
         self.owner_number = owner_number
-        self.treatment_notes = treatment_notes
+        self.treatment_notes = []
         self.current_vet = current_vet
+
+
+    def add_treatment_notes(self, notes):
+        self.treatment_notes.append(notes)
--- tests/animal_tests.py
+++ tests/animal_tests.py
@@ -5,7 +5,7 @@ from models.vet import Vet
 class TestAnimal(unittest.TestCase):
 
     def setUp(self):
-        self.animal1 = Animal("Garfield", "12/06/2018", "cat", 555679, "Too heavy, must diet to lose 2KG.")
+        self.animal1 = Animal("Garfield", "12/06/2018", "cat", 555679,)
         self.animal2 = Animal("George", "11/02/2011", "monkey", 456722)
 
         self.vet1 = Vet("Troy", "Barker", "Mon, Tues, Weds, Fri", "06/09/1991")
@@ -25,9 +25,19 @@ class TestAnimal(unittest.TestCase):
     def test_animal_owner_number(self):
         self.assertEqual(555679, self.animal1.owner_number)
 
-    def test_animal_treatment_notes(self):
-        self.assertEqual("Too heavy, must diet to lose 2KG.", self.animal1.treatment_notes)
-
     def test_animal_treatment_notes__no_notes(self):
         self.assertEqual([], self.animal2.treatment_notes)
 
+    def test_animal_adding_notes__already_notes(self):
+        self.animal1.add_treatment_notes("Garfield now needs to lose 4KG.")
+        self.assertEqual("Garfield now needs to lose 4KG.", self.animal1.treatment_notes[0])
+
+    def test_animal_adding_notes__empty(self):
+        self.animal2.add_treatment_notes("George is a curious boy.")
+        self.assertEqual("George is a curious boy.", self.animal2.treatment_notes[0])
+
+    def test_animal_adding_notes__add_multiple_notes(self):
+        self.animal1.add_treatment_notes("Garfield needs to lose 2KG")
+        self.animal1.add_treatment_notes("Garfield now needs to lose 4KG.")
+        self.assertEqual("Garfield needs to lose 2KG", self.animal1.treatment_notes[0])
+        self.assertEqual("Garfield now needs to lose 4KG.", self.animal1.treatment_notes[1])
