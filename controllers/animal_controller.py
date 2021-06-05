96f34e24e17643315100f90995148c1323ae1d0c
Gary Darnell <gdarnell9@gmail.com>
2021-06-05 21:48:36 +0100

    Created page and controller for create new animal record.

--- controllers/animal_controller.py
+++ controllers/animal_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from models.animal import Animal
 from models.vet import Vet
 import repositories.animal_repository as animal_repository
+import repositories.vet_repository as vet_repository
 
 animals_blueprint = Blueprint("animals", __name__)
 
@@ -16,3 +17,16 @@ def animals():
 def show(id):
     animal = animal_repository.select_by_id(id)
     return render_template("animals/show.html", animal = animal)
+
+@animals_blueprint.route("/animals/new")
+def new_animal():
+    vets = vet_repository.select_all_vets()
+    return render_template("animals/new.html", vets = vets)
+
+
+# @animals_blueprint.route("/animals, methods=['POST'])
+# def create_animal_record():
+#     name = request.form["name"]
+#     date_of_birth = request.form['date_of_birth']
+#     animal_type = request.form['animal_type']
+#     owner_number = request.form['']
