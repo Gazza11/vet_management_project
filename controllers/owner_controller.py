030d0f2b6871cd85752886aa384582371f212cff
Gary Darnell <gdarnell9@gmail.com>
2021-06-07 14:06:29 +0100

    Added new html for owners to add new owner. Trying to add button from create new animal to link straight to new owner.

--- controllers/owner_controller.py
+++ controllers/owner_controller.py
@@ -10,3 +10,26 @@ owners_blueprint = Blueprint("owners", __name__)
 def owners():
     owners = owner_repository.select_all()
     return render_template("owners/index.html", owners=owners)
+
+
+@owners_blueprint.route("/owners/<id>")
+def show(id):
+    owner = owner_repository.select_by_id(id)
+    return render_template("owners/show.html", owner = owner)
+
+
+@owners_blueprint.route('/owners/new')
+def new_owner():
+    return render_template("owners/new.html")
+
+
+@owners_blueprint.route("/owners", methods=['POST'])
+def create_owner():
+    name = request.form['name']
+    number = request.form['number']
+    address = request.form['address']
+    postcode = request.form['postcode']
+    registered = True
+    new_owner = Owner(name, number, address, postcode, registered)
+    owner_repository.save_owner(new_owner)
+    return redirect("/owners")
