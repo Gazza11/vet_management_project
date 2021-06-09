5440c95ef149dce3f17ee55871a101d03bf45e4f
Gary Darnell <gdarnell9@gmail.com>
2021-06-09 09:43:54 +0100

    Moved search again button to top of page on results.

--- db/vet_management.sql
+++ db/vet_management.sql
@@ -32,17 +32,30 @@ CREATE TABLE animals (
 --Below is used for populating the database for example purposes.
 INSERT INTO vets (first_name, last_name, working_days, date_of_birth) VALUES ('Troy', 'Barker', 'Mon, Tues', '1998-07-09');
 INSERT INTO vets (first_name, last_name, working_days, date_of_birth) VALUES ('Abed', 'Tanner', 'Mon, Weds', '1978-08-11');
+INSERT INTO vets (first_name, last_name, working_days, date_of_birth) VALUES ('Britta', 'Perry', 'Weds, Thurs, Fri', '1987-11-11');
+INSERT INTO vets (first_name, last_name, working_days, date_of_birth) VALUES ('Anthony', 'Rendon', 'Mon, Weds', '1978-08-11');
+INSERT INTO vets (first_name, last_name, working_days, date_of_birth) VALUES ('Karl', 'Malone', 'Mon, Tues, Weds, Thurs, Fri, Sat', '1964-09-18');
+INSERT INTO vets (first_name, last_name, working_days, date_of_birth) VALUES ('David', 'Fletcher', 'Tues, Weds, Thurs', '1997-08-11');
+INSERT INTO vets (first_name, last_name, working_days, date_of_birth) VALUES ('Jasmine', 'Fuller', 'Mon, Weds, Fri, Sun', '1978-08-11');
 
 
 INSERT INTO owners (name, number, address, postcode, registered) VALUES ('Liam Morrice', 998667, '13 Thistle Drive', 'AB43 6WB', true);
 INSERT INTO owners (name, number, address, postcode, registered) VALUES ('Doug Fraser', 909667, '9 Pencil Road', 'AB83 6MB', true);
 INSERT INTO owners (name, number, address, postcode, registered) VALUES ('Stu Christie', 998999, '78 Ember Drive', 'AB9 6WB', true);
 INSERT INTO owners (name, number, address, postcode, registered) VALUES ('Mike Trout', 272727, '27 Thistle Drive', 'AB27 6WB', false);
+INSERT INTO owners (name, number, address, postcode, registered) VALUES ('Farmer McGhee', 900432, 'The Farm Drive', 'EIE0 6WB', true);
+INSERT INTO owners (name, number, address, postcode, registered) VALUES ('Marcus Semien', 232466, '6 Toronto Avenue', 'AB27 6MS', true);
 
 
 INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Garfield', '2019-08-07', 'cat', 1, 'Garfield needs to lose weight.', 1);
 INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('George', '2020-07-14', 'monkey', 2, 'George is very Curious.', 1);
 INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Gregor', '2008-03-02', 'dog', 4, 'Gregor needs a course of anti-biotics.', 2);
+INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Silvester', '2009-03-02', 'cat', 3, 'Obsesses with mice.', 4);
+INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Emily', '2009-03-02', 'cat', 3, 'In good health.', 5);
+INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Ol Yella', '1999-03-02', 'dog', 1, 'Does not look good', 1);
+INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Emily', '2009-03-02', 'cat', 3, 'In good health.', 1);
+INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Emily', '2009-03-02', 'cat', 3, 'In good health.', 1);
+INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Emily', '2009-03-02', 'cat', 3, 'In good health.', 1);
 INSERT INTO animals (name, date_of_birth, animal_type, owner_details, treatment_notes, current_vet_id) VALUES ('Emily', '2009-03-02', 'cat', 3, 'In good health.', 1);
 
 
